165c7e4a668a27da18e8cd228f05f8dc9447ce33
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-06-10 03:45:16 -0400

    agrgue el escaneo de paquetes

--- src/main/java/com/edutech/ubicacion/UbicacionApplication.java
+++ src/main/java/com/edutech/ubicacion/UbicacionApplication.java
@@ -3,7 +3,7 @@ package com.edutech.ubicacion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.edutech"})
 public class UbicacionApplication {
 
 	public static void main(String[] args) {
